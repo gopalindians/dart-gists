79f94bdea623549ba22c6e89bbc112e1824df532
Gopal Sharma <gopalindians@gmail.com>
2020-05-12 22:13:09 +0530

    Added 008-DAY:dartfmt command line tool

---
+++ 008-DAY/dartfmt.dart
@@ -0,0 +1,14 @@
+/**
+ * @see:https://dart.dev/tools/dartfmt
+ * dartfmt command is used to format the code
+ */
+
+//use
+//dartfmt -w .
+//to format the code
+//the -w will format and overwriet the files
+
+void main() {
+  var names = [];
+  var upperLimit = 12;
+}
